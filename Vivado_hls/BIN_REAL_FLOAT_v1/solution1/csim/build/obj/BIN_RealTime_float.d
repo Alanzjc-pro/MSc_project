9d6580726446e917d48435ad211b105020e65e07
Alan <72474193+Alanzjc-pro@users.noreply.github.com>
2021-07-28 14:44:46 +0100

    Delete Vivado_hls/BIN_REAL_FLOAT_v1 directory

--- Vivado_hls/BIN_REAL_FLOAT_v1/solution1/csim/build/obj/BIN_RealTime_float.d
+++
@@ -1,3 +0,0 @@
-obj/BIN_RealTime_float.o: \
- ../../../../Compiled_Code/BIN_RealTime_float.cpp \
- ../../../../Compiled_Code/BIN_RealTime_float.h
--- Vivado_hls/BIN_REAL_FLOAT_v1/solution1/csim/build/obj/BIN_RealTime_float_test.d
+++
@@ -1,3 +0,0 @@
-obj/BIN_RealTime_float_test.o: \
- ../../../../Compiled_Code/BIN_RealTime_float_test.cpp \
- ../../../../Compiled_Code/BIN_RealTime_float.h
