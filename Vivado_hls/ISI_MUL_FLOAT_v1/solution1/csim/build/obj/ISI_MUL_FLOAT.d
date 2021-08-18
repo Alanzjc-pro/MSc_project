62473e038f3526f1dcb7c5740529abc7ff29a204
Alan <72474193+Alanzjc-pro@users.noreply.github.com>
2021-08-18 15:53:27 +0100

    Delete Vivado_hls directory

--- Vivado_hls/ISI_MUL_FLOAT_v1/solution1/csim/build/obj/ISI_MUL_FLOAT.d
+++
@@ -1,2 +0,0 @@
-obj/ISI_MUL_FLOAT.o: ../../../../Compiled_Code/ISI_MUL_FLOAT.cpp \
- ../../../../Compiled_Code/ISI_MUL_FLOAT.h
--- Vivado_hls/ISI_MUL_FLOAT_v1/solution1/csim/build/obj/ISI_MUL_FLOAT_test.d
+++
@@ -1,3 +0,0 @@
-obj/ISI_MUL_FLOAT_test.o: \
- ../../../../Compiled_Code/ISI_MUL_FLOAT_test.cpp \
- ../../../../Compiled_Code/ISI_MUL_FLOAT.h
